52e06335e7f3c1aa469a87d1d7d2e163c586906e
NataliyaShevchuk <nata.shevchuk@icloud.com>
2023-09-23 11:11:56 +0200

    added css and api call

---
+++ js/key.js
@@ -0,0 +1,9 @@
+export default function useApiKey() {
+	return "4a1a4a999ed0a5a1d2585585bf91e97a";
+};
+
+// For backend call:
+
+// create a .env file where to save the key of the API:
+
+// WEATHER_APP_API = "4a1a4a999ed0a5a1d2585585bf91e97a";
